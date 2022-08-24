307b77bd5f9a52950d0cfbc0b7dc7cb31d3ecf16
Midas Chien <midaschieh@google.com>
2022-08-24 21:02:03 +0800

    panel: common: add common control for LHBM HW constraints
    Move LHBM check to common control, include
    - not allow to turn on LHBM at non-peak refresh rate
    - warn an invalid attempt of refresh rate change when LHBM is on
    
    Bug: 243179526
    Test: LHBM
    Signed-off-by: Midas Chien <midaschieh@google.com>
    Change-Id: I56f0bed4c79ee9ce508fd8cc9ba51114b6555ac7

--- samsung/panel/panel-samsung-drv.h
+++ samsung/panel/panel-samsung-drv.h
@@ -480,6 +480,7 @@ struct exynos_panel_desc {
 	bool dbv_extra_frame;
 	bool is_partial;
 	bool is_panel_idle_supported;
+	bool no_lhbm_rr_constraints;
 	const unsigned int delay_dsc_reg_init_us;
 	const struct brightness_capability *brt_capability;
 	const u32 *bl_range;
@@ -576,6 +577,7 @@ struct exynos_panel {
 	 * at least the delay time provided after a refresh rate update.
 	 */
 	u32 idle_delay_ms;
+	int peak_vrefresh;
 
 	enum exynos_hbm_mode hbm_mode;
 	bool dimming_on;
